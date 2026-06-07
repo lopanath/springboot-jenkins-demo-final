a380ff1caac9ecffff87a157a58974c7a1c85cff
lopanath003 <lopanath003@gmail.com>
2026-06-07 17:11:02 +0530

    project added

---
+++ src/main/java/com/example/jenkinsdemo/JenkinsdemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.jenkinsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsdemoApplication.class, args);
+		System.out.println("Hi Lopa");
+	}
+
+}
